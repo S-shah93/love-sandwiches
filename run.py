8145cc61264dea10313d7009aae84739b4a8b33d
S-shah93 <sumair.shah@hotmail.com>
2023-01-03 12:13:14 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -52,29 +52,6 @@ def validate_data(values):
         return False
 
     return True
-""" 
-This code has been commented out as we have a cleaner function doing the same thing which is def update worksheet.
-Use this to help you refactor code.
-
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print("Updating surplus worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided.
-    """
-    print("Updating sales worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-"""
 
 
 def update_worksheet(data, worksheet):
@@ -107,6 +84,23 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting the last 5 entries 
+    for each sandwich and returns the data as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    # column = sales.col_values(3)
+    # print(column)
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    # pprint(columns)
+
+    return columns
+
 def main():
     """
     Run all program functions.
@@ -119,4 +113,7 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+
+sales_columns = get_last_5_entries_sales()
